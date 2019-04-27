e4d383023d1963d902c3e0c724da441b839038bc
Nowosad <tupiszakaczki@gmail.com>
2019-04-27 20:20:24 +0200

    update checks

--- R/palette_check.R
+++ R/palette_check.R
@@ -2,6 +2,7 @@
 #'
 #' @param x 
 #' @param tolerance 
+#' @param plot
 #' @param ... 
 #'
 #' @return
@@ -10,14 +11,15 @@
 #' @examples
 #' x = rcartocolor::carto_pal(11, "Vivid")
 #' palette_check(x)
+#' palette_check(x, plot = TRUE)
 #' palette_check(x, tolerance = 1)
-#' palette_check(x, tolerance = 10, metric = 2000)
-palette_check = function(x, tolerance = NULL, ...){
+#' palette_check(x, tolerance = 10, metric = 1976)
+palette_check = function(x, tolerance = NULL, plot = FALSE, ...){
 
   x_dist = palette_dist(x)
-  deu_dist = palette_dist(x, cvd = "deu")
-  pro_dist = palette_dist(x, cvd = "pro")
-  tri_dist = palette_dist(x, cvd = "tri")
+  deu_dist = palette_dist(x, cvd = "deu", ...)
+  pro_dist = palette_dist(x, cvd = "pro", ...)
+  tri_dist = palette_dist(x, cvd = "tri", ...)
   
   # number of colors
   nc = length(x)
@@ -37,11 +39,33 @@ palette_check = function(x, tolerance = NULL, ...){
   pro_ncp = ncp - sum(pro_dist < tolerance, na.rm = TRUE)
   tri_ncp = ncp - sum(tri_dist < tolerance, na.rm = TRUE)
   
-  data.frame(
+  # distance_hists = c(
+  #   spark_hist(x_dist),
+  #   spark_hist(deu_dist),
+  #   spark_hist(pro_dist),
+  #   spark_hist(tri_dist)
+  # )
+  
+  if (plot){
+    palette_plot(x)
+  }
+  
+  output = data.frame(
     name = c("normal", "deuteranopia", "protanopia", "tritanopia"),
     n = nc,
+    tolerance = tolerance,
     ncp = ncp,
-    ndcp = c(x_ncp, deu_ncp, pro_ncp, tri_ncp)
+    ndcp = c(x_ncp, deu_ncp, pro_ncp, tri_ncp),
+    min_dist = c(min(x_dist, na.rm = TRUE), min(deu_dist, na.rm = TRUE), min(pro_dist, na.rm = TRUE), min(tri_dist, na.rm = TRUE)),
+    mean_dist = c(mean(x_dist, na.rm = TRUE), mean(deu_dist, na.rm = TRUE), mean(pro_dist, na.rm = TRUE), mean(tri_dist, na.rm = TRUE)),
+    max_dist = c(max(x_dist, na.rm = TRUE), max(deu_dist, na.rm = TRUE), max(pro_dist, na.rm = TRUE), max(tri_dist, na.rm = TRUE))
+    # distances = distance_hists
   )
+  
+  oopts = options(digits = 2)
+  on.exit(options(oopts)) 
+  
+  output
+
 }
 
