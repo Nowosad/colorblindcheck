052426a5f51e64f08dcff5dc97edd6a5a1a9b689
Nowosad <tupiszakaczki@gmail.com>
2019-04-27 20:20:37 +0200

    switch to 2000 as the defualt

--- R/palette_dist.R
+++ R/palette_dist.R
@@ -2,7 +2,7 @@
 #'
 #' @param x 
 #' @param cvd 
-#' @param metric a vector of color metric specifiers. Valid values are '1976','1994', and '2000', which refer to the year the metric  was recommended by the CIE.
+#' @param metric a vector of color metric specifiers. Valid values are '1976', '1994', and '2000' (default), which refer to the year the metric  was recommended by the CIE.
 #' @param ...
 #' 
 #'
@@ -14,7 +14,7 @@
 #' palette_dist(x)
 #' palette_dist(x, cvd = "deu")
 #' 
-palette_dist = function(x, cvd = NULL, metric = 1976, ...){
+palette_dist = function(x, cvd = NULL, metric = 2000, ...){
   
   if (!is.null(cvd)){
     x = switch(
