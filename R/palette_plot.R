6674f6006103d54a0af9064440e83e9009b01c1b
Nowosad <tupiszakaczki@gmail.com>
2020-08-14 13:12:21 +0200

    adds palette_bivariate_plot

--- R/palette_plot.R
+++ R/palette_plot.R
@@ -6,6 +6,8 @@
 #' @param severity Severity of the color vision defect, a number between 0 and 1
 #' 
 #' @importFrom graphics par plot rect text
+#' 
+#' @seealso palette_bivariate_plot
 #'
 #' @export
 #'
@@ -62,3 +64,52 @@ palette_plot = function(x, severity = 1){
     adj = 1
   )
 }
+
+#' Plot Bivariate Palette And Its Color Vision Deficiencies
+#' 
+#' Plot of the original input bivariate palette and simulations of color vision deficiencies - deuteranopia, protanopia, and tritanopia.
+#'
+#' @param x A vector of hexadecimal color descriptions
+#' @param severity Severity of the color vision defect, a number between 0 and 1
+#' 
+#' @seealso palette_plot
+#'
+#' @export
+#'
+#' @examples
+#' palette_bivariate_plot(x = rcartocolor::carto_pal(4, "Sunset"))
+palette_bivariate_plot = function(x, severity = 1){
+  deu = colorspace::deutan(x, severity = severity)
+  pro = colorspace::protan(x, severity = severity)
+  tri = colorspace::tritan(x, severity = severity)
+  
+  y = rev(list(tri, pro, deu, x))
+  labels = rev(c("Tritanopia", "Protanopia", "Deuteranopia", "Normal"))
+  
+  my_n = length(x)
+  n_colors = 4
+  ylim = c(0, n_colors)
+  
+  ncol = length(x)
+  # if(missing(nx)) {
+    nx = sqrt(ncol)
+  # }
+  # if(missing(ny)) {
+    ny = nx
+  # }
+  
+  oldpar = par(mfrow = c(2, 2), mar = rep(1, 4))
+  on.exit(par(oldpar))
+  
+  for (i in seq_len(n_colors)) {
+    graphics::image(
+      matrix(1:ncol, nrow = ny),
+      axes = FALSE,
+      col = y[[i]],
+      main = labels[[i]],
+      asp = 1
+    )
+  }
+
+}
+
